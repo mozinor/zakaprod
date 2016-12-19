a7716d36820cad7d5e6517f74093d43f4fbc5056
guyfawkes <guyfawkes@localhost.localdomain>
2016-12-19 01:52:33 -0500

    Zaka Commit

--- zakaprod/settings.py
+++ zakaprod/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v9f0kkqtjt_xqc!4d_d8$5nirktg^b^qdcm#2hdes_^k)ahwl('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
